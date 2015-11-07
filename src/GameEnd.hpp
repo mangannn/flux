2ebdc83266cc805c572507b60a640a73cf5d3b5b
Magnus Fries <magnus.fries@gmail.com>
2015-11-07 13:43:10 +0100

    Forgot to track some files (one more)

---
+++ src/GameEnd.hpp
@@ -0,0 +1,78 @@
+#include "EventPass.hpp"
+
+#include <math.h>
+
+class GameEnd : public EventPass {
+
+public:
+	
+	float timer;
+	float duration;
+
+	sf::Color color;
+
+	GameEnd(float durationParam = 1.0f) :
+		timer(0.0f),
+		duration(durationParam),
+
+		color(0xff, 0, 0, 0)
+	{}
+	virtual ~GameEnd() {}
+
+
+	virtual EventPass *eventHandle(sf::Event event) {
+
+		switch (event.type) {
+			case sf::Event::KeyPressed: {
+				switch (event.key.code) {
+					case sf::Keyboard::Return: {
+						return new CharacterSelect();
+					} break;
+					default: break;
+				}
+			} break;
+			case sf::Event::MouseButtonPressed: {
+				switch (event.mouseButton.button) {
+					case sf::Mouse::Left: {
+						return new CharacterSelect();
+					} break;
+					default: break;
+				}
+			} break;
+			default: break;
+		}
+
+		return NULL;
+	}
+
+	virtual EventPass *update(float elapsedTime) {
+
+		timer += elapsedTime;
+
+		float x = (timer / duration);
+
+		float y = (-2.0 * (x * x * x)) + (3.0 * (x * x));
+
+		color.a = 0xff * y;
+
+		color.r = 200 * (1 - y);
+
+		if (timer > duration) {
+			return new CharacterSelect();
+		} else {
+			return NULL;
+		}
+	}
+
+	virtual void draw(RenderWindow *window) {
+
+
+		sf::RectangleShape box(window->getView().getSize());
+		box.setFillColor(color);
+
+		window->draw(box);
+
+
+		//window->clear(color);
+	}
+};
